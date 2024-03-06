22aa8c8677229173df27a0986f31426667423a2d
soleblaze <soleblaze@skyshadows.net>
2024-03-05 23:14:40 -0600

    Fix markdown auto wrapping

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,6 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    format_on_save = function()
-      if vim.g.autoFormat then
-        return { timeout_ms = 500, lsp_fallback = true }
-      end
-    end,
     formatters_by_ft = {
       ["*"] = { "trim_whitespace" },
       ["json"] = { "fixjson" },
@@ -17,6 +12,9 @@ return {
     formatters = {
       shfmt = {
         prepend_args = { "-ci", "-s", "-i", "2", "-bn", "-sr" },
+      },
+      mdformat = {
+        prepend_args = { "--wrap", "80" },
       },
     },
     format = {
