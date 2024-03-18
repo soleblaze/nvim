5a4c824fbf074907d9dee6c76920081e8b46a863
soleblaze <soleblaze@skyshadows.net>
2024-03-18 16:08:38 -0500

    Fix cobalt2 theme

--- lua/plugins/cobalt2.lua
+++ lua/plugins/cobalt2.lua
@@ -2,7 +2,7 @@ return {
   {
     "lalitmee/cobalt2.nvim",
     event = { "ColorSchemePre" },
-    dependencies = { "tjdevries/colorbuddy.nvim" },
+    dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
     init = function()
       require("colorbuddy").colorscheme("cobalt2")
     end,
